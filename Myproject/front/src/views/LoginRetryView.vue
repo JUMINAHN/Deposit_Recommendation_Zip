389e2aec0441656445a1e67380870468d2cb9977
JUMINAHN <jumin9774@naver.com>
2024-11-20 00:02:22 +0900

    로그인 : DRF 연동 코드 구현, 아직 실행은 하지 않았지만 코드 주석 처리 완료하였음 추가적으로 더미 데이터를 통해 구동 확인 완료함 두번째로 비밀번호 입력시 visible 관련 기능 추가 세번째로 비밀번호를 까먹었을 때 OTP 창을 통해 임시 처리 진행이 가능하도록 추가 완료

---
+++ Myproject/front/src/views/LoginRetryView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="topClass">
+    <v-card
+    class="py-12 px-8 text-center mx-auto ma-4"
+    max-width="420"
+    width="100%"
+  >
+    <h3 class="text-h6 mb-2">
+      Please enter the one time password to verify your account
+    </h3>
+
+    <div>The code is 240132</div>
+
+    <v-otp-input
+      v-model="otp"
+      :disabled="validating"
+      color="blue"
+      variant="plain"
+    ></v-otp-input>
+
+    <v-btn
+      :loading="validating"
+      class="mt-6 text-none bg-surface-variant"
+      height="40"
+      text="Validate"
+      variant="plain"
+      width="135"
+      border
+      rounded
+      @click.prevent="checkNumber"
+      ></v-btn>
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+  const router = useRouter()
+  //숫자불일치시 alert : 재입력
+  const otp = ref(null)
+  const checkNumber = function() {
+    if (otp.value === '240132') { //맞추면 임시로 mainpage 이동
+      alert('인증되었습니다.')
+      router.push({name : 'main'})
+    } else {
+      alert('인증에 실패했습니다.')
+    }
+  }
+
+
+</script>
+
+<style scoped>
+  .topClass {
+    margin-top:150px;
+  }
+</style>
