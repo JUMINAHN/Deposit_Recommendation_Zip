3982fd5b6aa4aa062b4214247347daf0950f67e9
JUMINAHN <jumin9774@naver.com>
2024-11-19 16:03:19 +0900

    update files : drf == django files >> CRUD updates : 게시판 기능 업데이트(추후 좋아요랑 댓글 기능 구현 필요, 아직 User 기능 미구현)

---
+++ Myproject/back/bank/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'bank'
+urlpatterns = [
+    path('', views.article_list_create), #메인 화면 == article
+    path('<int:pk>/', views.article_detail), #어떠한 몇 번째 article?
+    path('<int:pk>/comments/', views.comment_create), #몇 번째 article에 대한 댓글
+    path('comments/', views.article_comment), #댓글 생성 => 번호 부여
+    path('comments/<int:comment_pk>/', views.article_comment_detail), #상세 댓글 조회
+    ]
