05de1883a562d3fbaf7f2d5ce69393786cb91b1e
JUMINAHN <jumin9774@naver.com>
2024-11-19 11:24:40 +0900

    update axios move : recommendedView => store : bank.js // 현재 RecoomendView에서 items 이슈가 종종 발생하는 듯함 => 현재는 해결했으나 추후 이슈 발생시 검토할 필요가 있음, 헷갈림

---
+++ Myproject/front/src/stores/bank.js
@@ -0,0 +1,72 @@
+import { ref, computed, reactive } from 'vue'
+import { defineStore } from 'pinia'
+
+
+export const useBankStore = defineStore('bank', () => {
+  //더미 데이터를 생성해서 대략적인 틀을 잡아갈 수 있음
+  //일단은 메인 data만 다뤄보자 => 3개만 만들어보자
+  //순위, 6개월, 12개월, 세전이자, 세후이자, 24개월, 36개월, 금융기관, 상품, update
+
+  //더미 데이터 그자체를 ..
+  const dummyData = reactive([
+    {'순위': 1,
+    '6개월': 1.30,
+    '12개월': 3.90,
+    '세전이자': 390000,
+    '세후이자': 329940,
+    '24개월': 1.50,
+    '36개월': 1.50,
+    '금융기관': 'MG 보은',
+    '상품': 'Block예금',
+    'update': '11.18'
+    },
+    {'순위': 2,
+    '6개월': '-',
+    '12개월': 3.90,
+    '세전이자': 390000,
+    '세후이자': 329940,
+    '24개월': '-',
+    '36개월': '-',
+    '금융기관': 'MG 보은',
+    '상품': 'MG 더뱅킹 정기예금',
+    'update': '10.28'
+    },
+    {'순위': 3,
+    '6개월': '-',
+    '12개월': 3.90,
+    '세전이자': 380000,
+    '세후이자': 321480,
+    '24개월': '-',
+    '36개월': '-',
+    '금융기관': '봉화 신협',
+    '상품': '파워정기예탁금',
+    'update': '11.18'
+    },
+  ])
+
+  //user가 입력한 예치금 데이터
+  const userDepositData = ref(null)
+  //v-model로 추가 유저 데이터 입력값 받아오기
+  const getUserData = function(data){
+    userDepositData.value = data //data 자체를 받아오기
+  }
+
+  //정기 예금 검색 조건들 => 더미 데이터, 추가적으로 입력하게 될 데이터들
+  //title, content부분 받아와서 진행할 필요 있음
+  let id = 1
+  const findCondition = reactive([
+    //option에 사용될 데이터들..
+    {id: id++, title: '세율', content: ['일반과세', '세금우대', '비과세']},
+    {id: id++, title: '이자지급방식', content: ['전체', '만기지급', '월지급']},
+    {id: id++, title: '가입방식', content: ['전체', '온라인', '방문']},
+    //예치금액은 input받는 값 => v-model로 input받고 또 던져줘야 함
+    //편의를 위해 이것도 동일하게 생성
+    {id: id++, title: '예치금액', content: userDepositData},
+    {id: id++, title: '예치기간', content: ['6개월', '12개월', '24개월', '36개월']},
+    //추후 데이터 넣을 것 현재는 더미데이터
+    {id: id++, title: '지역', content: ['서울', '부산', '경기', '인천', '포항']},
+  ])
+
+
+  return {  dummyData, findCondition, getUserData  }
+})
--- Myproject/front/src/views/RecommendView.vue
+++ Myproject/front/src/views/RecommendView.vue
@@ -5,13 +5,14 @@
         <v-container fluid>
           <v-row>
             <v-col cols="2"
-            v-for="find in findCondition"
+            v-for="(find, index) in findCondition"
             :key="find.id">
               <v-combobox
-                v-model="value"
-                :items="find.content"
+                v-model="find.selectedValue"
+                :items="find.content || []"
                 :label="find.title"
               ></v-combobox>
+              <!--                 :items="Array.isArray(find.content) ? find.content : []" -->
             </v-col>
           </v-row>
         </v-container>
@@ -50,7 +51,7 @@
         <!--전체 데이터 -->
         <v-data-table
           :headers="headers"
-          :items="dummyData"
+          :items="items"
           :search="search"
         ></v-data-table>
       </v-card>
@@ -59,61 +60,23 @@
 </template>
 
 <script setup>
-  //더미 데이터를 생성해서 대략적인 틀을 잡아갈 수 있음
-  //일단은 메인 data만 다뤄보자 => 3개만 만들어보자
-  //순위, 6개월, 12개월, 세전이자, 세후이자, 24개월, 36개월, 금융기관, 상품, update
-  const dummyData = [
-    {'순위': 1,
-    '6개월': 1.30,
-    '12개월': 3.90,
-    '세전이자': 390000,
-    '세후이자': 329940,
-    '24개월': 1.50,
-    '36개월': 1.50,
-    '금융기관': 'MG 보은',
-    '상품': 'Block예금',
-    'update': '11.18'
-    },
+import { useBankStore } from '@/stores/bank'
+import { computed, ref } from 'vue'
+const store = useBankStore()
 
-    {'순위': 2,
-    '6개월': '-',
-    '12개월': 3.90,
-    '세전이자': 390000,
-    '세후이자': 329940,
-    '24개월': '-',
-    '36개월': '-',
-    '금융기관': 'MG 보은',
-    '상품': 'MG 더뱅킹 정기예금',
-    'update': '10.28'
-    },
-    
-    {'순위': 3,
-    '6개월': '-',
-    '12개월': 3.90,
-    '세전이자': 380000,
-    '세후이자': 321480,
-    '24개월': '-',
-    '36개월': '-',
-    '금융기관': '봉화 신협',
-    '상품': '파워정기예탁금',
-    'update': '11.18'
-    },
-  ]
+//selectValue를 ..? 흠
+const findCondition = store.findCondition
 
-  //정기 예금 검색 조건들
-  let id = 1
-  const findCondition = [
-    //option에 사용될 데이터들..
-    {id: id++, title: '세율', content: ['일반과세', '세금우대', '비과세']},
-    {id: id++, title: '이자지급방식', content: ['전체', '만기지급', '월지급']},
-    {id: id++, title: '가입방식', content: ['전체', '온라인', '방문']},
-    //예치금액은 input받는 값 => v-model로 input받고 또 던져줘야 함
-    //편의를 위해 이것도 동일하게 생성
-    {id: id++, title: '예치금액', content: [1000, 5000, 10000, 15000, 20000, 30000, 100000]},
-    {id: id++, title: '예치기간', content: ['6개월', '12개월', '24개월', '36개월']},
-    //추후 데이터 넣을 것 현재는 더미데이터
-    {id: id++, title: '지역', content: ['서울', '부산', '경기', '인천', '포항']},
-  ]
+
+//data table관련해서 
+const dummyKey = ['순위', '6개월', '12개월', '세전이자', '세후이자', '금융기관', '상품', 'update']
+const headers = dummyKey.map(item => ({
+  title: item,
+  key: item
+}))
+const items = computed(() => store.dummyData)
+const search = ref('')
+
 
 </script>
 
